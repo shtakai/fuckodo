7048c6997dec5a5cc57376024e0cbc9d670c9312
Alyson Takai <shtakai@gmail.com>
2018-05-24 21:55:46 +0700

    Modified

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      {
+        label: 'New Todo',
+        click() { createAddWindow() }
+      },
       {
         label: 'Quit',
         accelerator: (() => {
@@ -28,12 +31,38 @@ if (process.platform === 'darwin') {
   menuTemplate.unshift({})
 }
 
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
+
 let mainWindow
+let addWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow.on('closed', () => app.quit())
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(mainMenu)
 })
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  })
+  addWindow.loadURL(`file://${__dirname}/add.html`)
+}
