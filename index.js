b5f5018a6347a08cb9d27655e0e4777599fad54c
Alyson Takai <shtakai@gmail.com>
2018-05-24 20:37:12 +0700

    Add bootstrapping

---
+++ index.js
@@ -0,0 +1,9 @@
+const electron = require('electron')
+
+const { app, BrowserWindow, Menu } = electron
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({})
+  mainWindow.loadURL(`file://${__dirname}/main.html`)
+})
