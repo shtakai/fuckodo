ad3575b599e82f717e59809ac8600a7d620c546f
Alyson Takai <shtakai@gmail.com>
2018-05-24 21:21:27 +0700

    Add menu option

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const electron = require('electron')
 
 const { app, BrowserWindow, Menu } = electron
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo' },
+      {
+        label: 'Quit',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Command+Q'
+          } else {
+            return 'Ctrl+Q'
+          }
+        })(),
+        click() {
+          app.quit()
+        }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({})
+}
+
 let mainWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(mainMenu)
 })
